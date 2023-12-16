9d2f442da85026c7dda67336103d1fabca913b2c
Malikov Alexander <109045884+whoopzee1337@users.noreply.github.com>
2023-12-16 21:34:15 +0300

    Revert "Add files via upload (#3)" (#4)
    This reverts commit 89c2c9bc32be6dabbcb8b6b6e9c0503c1ac8ad8d.

--- src/main/java/org/mts/lab1/Application.java
+++ src/main/java/org/mts/lab1/Application.java
@@ -1,9 +1,12 @@
 package org.mts.lab1;
 
-import org.mts.lab1.dto.SimpleShop;
+import org.mts.lab1.entity.SimpleShop;
 import org.mts.lab1.service.SimpleShopCalculationsService;
 
 public class Application {
     public static void main(String[] args) {
+        System.out.println(SimpleShopCalculationsService.calculations(new SimpleShop(15, 1000, 0.75)));
+        System.out.println(SimpleShopCalculationsService.calculations(new SimpleShop(15, 1000, 42.575)));
+        System.out.println(SimpleShopCalculationsService.calculations(new SimpleShop(15, 1000, 59.1)));
     }
 }
--- src/main/java/org/mts/lab2/abstracts/Pet.java
+++
@@ -1,7 +0,0 @@
-package org.mts.lab2.abstracts;
-
-import org.mts.lab2.abstracts.parent.AbstractAnimal;
-
-public abstract class Pet extends AbstractAnimal {
-
-}
