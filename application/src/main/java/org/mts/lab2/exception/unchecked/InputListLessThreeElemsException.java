17dd9d6d3a33ba40fb3a359e7a5190d81cd98277
BogdanTeryukhov <94743986+BogdanTeryukhov@users.noreply.github.com>
2024-05-14 13:50:03 +0300

    Delete application directory

--- application/src/main/java/org/mts/lab2/exception/unchecked/InputListLessThreeElemsException.java
+++
@@ -1,6 +0,0 @@
-package org.mts.lab2.exception.unchecked;
-
-public class InputListLessThreeElemsException extends UnknownError{
-    //private static final String message = "Input list has less than 3 elements!";
-    public InputListLessThreeElemsException() {}
-}
