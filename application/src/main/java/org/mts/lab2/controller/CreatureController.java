a594a1a7bb5b77ae59054dca1dcd1a25dd91099a
BogdanTeryukhov <94743986+BogdanTeryukhov@users.noreply.github.com>
2024-05-17 19:00:19 +0300

    added CreatureDTO

--- application/src/main/java/org/mts/lab2/controller/CreatureController.java
+++ application/src/main/java/org/mts/lab2/controller/CreatureController.java
@@ -1,6 +1,8 @@
 package org.mts.lab2.controller;
 
 import jakarta.annotation.PostConstruct;
+import org.modelmapper.ModelMapper;
+import org.mts.dto.CreatureDTO;
 import org.mts.entity.Creature;
 import org.mts.repository.AnimalTypeRepository;
 import org.mts.repository.CreatureRepository;
@@ -10,16 +12,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.stream.Collectors;
+
 @Controller
 public class CreatureController {
 
     @Autowired
     private CreatureService creatureService;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
 
     @GetMapping("/allCreatures")
     private String getCreatures(Model model){
-        model.addAttribute("creatures", creatureService.findAllCreatures());
+        model.addAttribute("creatures",
+                creatureService
+                        .findAllCreatures()
+                        .stream()
+                        .map(creature -> modelMapper.map(creature, CreatureDTO.class))
+                        .collect(Collectors.toList()));
         return "allCreatures";
     }
 
@@ -31,18 +43,19 @@ public class CreatureController {
 
     @GetMapping("/addCreature")
     private String addNew(Model model){
-        model.addAttribute("newCreature", new Creature());
+        model.addAttribute("newCreature", new CreatureDTO());
         return "addCreature";
     }
 
     @PostMapping(value = "/addCreature", params = "action=add")
-    private String addCreature(@ModelAttribute("newCreature") Creature creature){
+    private String addCreature(@ModelAttribute("newCreature") CreatureDTO creatureDTO){
+        Creature creature = modelMapper.map(creatureDTO, Creature.class);
         creatureService.saveCreature(creature);
         return "redirect:/allCreatures";
     }
 
     @PostMapping(value = "/addCreature", params = "action=cancel")
-    private String noAddCreature(@ModelAttribute("newCreature") Creature creature){
+    private String noAddCreature(@ModelAttribute("newCreature") CreatureDTO creatureDTO){
         return "redirect:/allCreatures";
     }
 }
--- application/src/main/java/org/mts/lab2/controller/RestCreatureController.java
+++ application/src/main/java/org/mts/lab2/controller/RestCreatureController.java
@@ -1,18 +1,39 @@
 package org.mts.lab2.controller;
 
+import org.modelmapper.ModelMapper;
+import org.mts.dto.CreatureDTO;
 import org.mts.entity.Creature;
 import org.mts.service.CreatureService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.ParseException;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 public class RestCreatureController {
 
-    @Autowired
-    private CreatureService creatureService;
+    private final CreatureService creatureService;
+    private final ModelMapper modelMapper;
 
-    @GetMapping("/rest/addCreature")
-    private String addRestCreature(@RequestBody Creature creature) {
+    public RestCreatureController(CreatureService creatureService, ModelMapper modelMapper) {
+        this.creatureService = creatureService;
+        this.modelMapper = modelMapper;
+    }
+
+    @GetMapping("/rest/allCreatures")
+    public List<CreatureDTO> getAllCreatures() {
+        return creatureService
+                .findAllCreatures()
+                .stream()
+                .map(creature -> modelMapper.map(creature, CreatureDTO.class))
+                .collect(Collectors.toList());
+    }
+
+    @PostMapping("/rest/addCreature")
+    private String addRestCreature(@RequestBody CreatureDTO creatureDTO) {
+        Creature creature = modelMapper.map(creatureDTO, Creature.class);
         creatureService.saveCreature(creature);
         return "Creature added";
     }
@@ -22,4 +43,5 @@ public class RestCreatureController {
         creatureService.deleteCreatureById(id);
         return "Creature has been deleted";
     }
+
 }
